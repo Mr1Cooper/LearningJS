3792892e1d208e841ab5646fdf93cdc7a05b55c6
Andrii <enjoyitalbion@gmail.com>
2019-03-03 21:34:06 +0100

    Перший коміт

---
+++ Task1.js
@@ -0,0 +1,12 @@
+let person = {
+    name: "Ivan",
+    age: "35",
+    isMarried: false
+};
+console.log(person.name);
+
+let answer = confirm("Who are you?");
+
+console.log(answer);
+
+console.log(55%5);
