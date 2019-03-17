d412e1785abfcdf0d791b6ac43a06356ad86e6b5
Andrii <enjoyitalbion@gmail.com>
2019-03-17 15:26:13 +0100

    Перше завдання

--- Task1.js
+++ Task1.js
@@ -1,12 +1,21 @@
-let person = {
-    name: "Ivan",
-    age: "35",
-    isMarried: false
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
 };
-console.log(person.name);
 
-let answer = confirm("Who are you?");
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
 
-console.log(answer);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-console.log(55%5);
+alert(appData.budget / 30);
